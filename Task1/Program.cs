7750ae8cae13ffae1785a6a7e4ef125057d0ffac
mila <milavoronina1997@mail.ru>
2022-08-26 20:25:17 +0500

    Task1_1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,49 +5,27 @@
 //12821 -> да
 
 //23432 -> да
-//string Palindrom(string x)
-//{
-    //if (x[0] == x[4] && x[1] == x[3])
-    //{
-        //Console.WriteLine(" This is a five-digit number");
-    //}
 
-    //return(x);
-//}
-//try
-//{
-
-    //Console.WriteLine("Enter a five-digit number");
-    //string a = Console.ReadLine();
-    //Console.WriteLine(Palindrom (a));
-//}
-//catch
-//{
-   // Console.WriteLine(" You entered not a five-digit number");
-//}
-
-
-// Second way:
-
-void Palindrom()
-{
-    Console.WriteLine("Enter a five-digit number");
-    string x = Console.ReadLine();
+string Palindrom (string x)
+{ 
     int y = int.Parse(x); 
     if (y >= 10000 && y < 100000)
-        if (x[0] == x[4] && x[1] == x[3])
-        {
-            Console.WriteLine(" This is a palindrom");
-        }
-        else
-        {
-            Console.WriteLine(" This is NOT a palindrom");
-        }
-    else
     {
-        Console.WriteLine(" You should enter a five-digit number");
+        if (x[0] == x[4] && x[1] == x[3]) return " This is a palindrom";
+        else return "This is NOT a palindrom";    
     }
+    else return "You should enter a five-digit number";
 }
+ try
+ {
+    Console.WriteLine(" Enter a five - digit number");
+    string x = Console.ReadLine();
+    Console.WriteLine(Palindrom(x));
+ }
+ catch 
+ {
+    Console.WriteLine("You should enter a five-digit number");
 
-Palindrom();
+ }
+
     
