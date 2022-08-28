b7d7b6b912540a3f20f9ea7a86adf08237442a75
mila <milavoronina1997@mail.ru>
2022-08-28 17:35:21 +0500

    Task4_1

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -5,3 +5,18 @@
 //В примере мы считываем числа 1, -3, 5, -6, -10, 13; в этот момент замечаем, что сумма этих чисел равна нулю и выводим сумму их 
 //квадратов, не обращая внимания на то, что остались ещё не прочитанные значения.
 
+int sum = 0;
+int sum2 = 0;
+int n = 1;
+while (n != 0)
+{
+    Console.WriteLine("Enter you number");
+    int n = Convert.ToInt32(Console.ReadLine());
+    sum  = sum + n;
+    sum2 = (sum2 + (n*n));
+    if (sum == 0)
+    break;
+}
+Console.WriteLine(sum);
+Console.Write(sum2);
+
